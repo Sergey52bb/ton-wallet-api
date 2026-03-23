2cd6cc2fa6057c1ef3c30bdf6664ec3b34b7f3f3
Sergey52bb <149534508+Sergey52bb@users.noreply.github.com>
2026-03-23 22:31:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,14 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-const TOKEN = '8715661147:AAGPQcf5RNvaWA57Y2yQxbLYyUE39liMnSU'; // ← вставь свой токен
+const TOKEN = 'ТВОЙ_ТОКЕН'; // вставь сюда свой токен
 const WEBAPP_URL = 'https://sergey52bb.github.io/grok-autotrade/';
 
 const bot = new TelegramBot(TOKEN, { polling: true });
 
-// команда старт
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
 
-  bot.sendMessage(
-    chatId,
+  bot.sendMessage(chatId,
     '💎 SYN TERMINAL\nAI Trading System\n\n🚀 Открыть терминал:',
     {
       reply_markup: {
